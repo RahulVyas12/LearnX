10f8e906564ba17240be0f5b62d89987bcc9c02c
RahulVyas12 <vyasr7972@gmail.com>
2026-02-23 21:39:23 +0530

    created Login and register page

---
+++ Controllers/screensController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LearnX.Controllers
+{
+    public class screensController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult LoginPage()
+        {
+            return View();
+        }
+        public IActionResult RegisterPage()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/AccountModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LearnX.Models
+{
+    public class AccountModel
+    {
+        public string name { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        public string email { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        public string password { get; set; }
+
+    }
+}
