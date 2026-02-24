ac1c0da65c22291889feb77558db94c48b4eb881
RahulVyas12 <vyasr7972@gmail.com>
2026-02-24 12:42:39 +0530

    feat: add dashboard layout, screens controller, and progress report view with dedicated styling.

--- Controllers/screensController.cs
+++ Controllers/screensController.cs
@@ -25,5 +25,10 @@ namespace LearnX.Controllers
         {
             return View();
         }
+
+        public IActionResult ProgressReport()
+        {
+            return View();
+        }
     }
 }
