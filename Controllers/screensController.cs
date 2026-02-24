b0c299eb6e4622b77fca0617efd8d3c4a38c669b
RahulVyas12 <vyasr7972@gmail.com>
2026-02-24 10:41:21 +0530

    feat: Implement initial application structure, including main layout, dashboard, and login pages.

--- Controllers/screensController.cs
+++ Controllers/screensController.cs
@@ -1,18 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LearnX.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LearnX.Controllers
 {
     public class screensController : Controller
     {
+
         public IActionResult Index()
         {
+            
             return View();
         }
         public IActionResult LoginPage()
         {
             return View();
         }
+
         public IActionResult RegisterPage()
+        {
+            return View();
+        }
+
+        public IActionResult Dashboard()
         {
             return View();
         }
