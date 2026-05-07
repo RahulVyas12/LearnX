7ba68854f50267b03d341b7aeac92d910e31658a
vyas60594 <vyas60594@gmail.com>
2026-05-07 23:24:05 +0530

    feat: add AdminSkillPathEditor component and initial gitignore file

--- frontend/src/pages/admin/AdminSkillPathEditor.jsx
+++ frontend/src/pages/admin/AdminSkillPathEditor.jsx
@@ -3,7 +3,7 @@ import { useParams, useNavigate } from 'react-router-dom';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import toast from 'react-hot-toast';
 import adminService from '../../services/adminService';
-import '../styles/scrollbar.css';
+import './styles/scrollbar.css';
 
 const AdminSkillPathEditor = () => {
     const { id } = useParams();
@@ -643,11 +643,12 @@ const AdminSkillPathEditor = () => {
                                                                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                                                                                 </svg>
                                                                             </button>
-                                                                        </div>
                                                                     </div>
                                                                 </div>
-                                                            )}
-                                                        </Draggable>
+                                                            </div>
+                                                        </div>
+                                                    )}
+                                                </Draggable>
                                                 ))}
                                                 {provided.placeholder}
                                             </div>
